a3b66da84553d80db9a1d0420f6859d4d457a96f
Tastaturberuf <mail@tastaturberuf.de>
2025-10-11 19:04:54 +0200

    Introduce fluent setter methods for Config properties
    - Added fluent setter methods for various Config properties to simplify and streamline usage.
    - Replaced direct references to `$this->table` with `$this->_table` for consistency and immutability.
    - Enhanced constructor to initialize `_table` separately and maintain `readonly` behavior.

--- src/Config.php
+++ src/Config.php
@@ -13,6 +13,7 @@ use Tastaturberuf\ContaoDataContainerAccessor\Callback\ConfigCallback;
 #[AllowDynamicProperties]
 final class Config extends DynamicPropertiesInterface
 {
+    private readonly string $_table;
 
     /**
      * The label is used with page or file trees and typically includes reference to the language array.
@@ -22,6 +23,13 @@ final class Config extends DynamicPropertiesInterface
         set {
             $this->__set('label', $value);
         }
+    }
+
+    public function label(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
     }
 
     /**
@@ -34,11 +42,25 @@ final class Config extends DynamicPropertiesInterface
         }
     }
 
+    public function ptable(string $ptable): self
+    {
+        $this->ptable = $ptable;
+
+        return $this;
+    }
+
     public ?bool $dynamicPtable {
         get => $this->__get('dynamicPtable');
         set {
             $this->__set('dynamicPtable', $value);
         }
+    }
+
+    public function dynamicPtable(bool $dynamicPtable = true): self
+    {
+        $this->dynamicPtable = $dynamicPtable;
+
+        return $this;
     }
 
     public ?array $ctable {
@@ -48,11 +70,25 @@ final class Config extends DynamicPropertiesInterface
         }
     }
 
+    public function ctable(array $ctable): self
+    {
+        $this->ctable = $ctable;
+
+        return $this;
+    }
+
     public ?string $dataContainer {
         get => $this->__get('dataContainer');
         set {
             $this->__set('dataContainer', $value);
         }
+    }
+
+    public function dataContainer(string $dataContainer = 'Contao\DC_Table'): self
+    {
+        $this->dataContainer = $dataContainer;
+
+        return $this;
     }
 
     public ?string $markAsCopy {
@@ -62,11 +98,25 @@ final class Config extends DynamicPropertiesInterface
         }
     }
 
+    public function markAsCopy(string $markAsCopy): self
+    {
+        $this->markAsCopy = $markAsCopy;
+
+        return $this;
+    }
+
     public ?string $uploadPath {
         get => $this->__get('uploadPath');
         set {
             $this->__set('uploadPath', $value);
         }
+    }
+
+    public function uploadPath(string $uploadPath): self
+    {
+        $this->uploadPath = $uploadPath;
+
+        return $this;
     }
 
     public ?string $validFileTypes {
@@ -76,11 +126,25 @@ final class Config extends DynamicPropertiesInterface
         }
     }
 
+    public function validFileTypes(string $validFileTypes): self
+    {
+        $this->validFileTypes = $validFileTypes;
+
+        return $this;
+    }
+
     public ?string $editableFileTypes {
         get => $this->__get('editableFileTypes');
         set {
             $this->__set('editableFileTypes', $value);
         }
+    }
+
+    public function editableFileTypes(string $editableFileTypes): self
+    {
+        $this->editableFileTypes = $editableFileTypes;
+
+        return $this;
     }
 
     public ?bool $databaseAssisted {
@@ -90,11 +154,25 @@ final class Config extends DynamicPropertiesInterface
         }
     }
 
+    public function databaseAssisted(bool $databaseAssisted = true): self
+    {
+        $this->databaseAssisted = $databaseAssisted;
+
+        return $this;
+    }
+
     public ?bool $closed {
         get => $this->__get('closed');
         set {
             $this->__set('closed', $value);
         }
+    }
+
+    public function closed(bool $closed = true): self
+    {
+        $this->closed = $closed;
+
+        return $this;
     }
 
     public ?bool $notEditable {
@@ -104,11 +182,25 @@ final class Config extends DynamicPropertiesInterface
         }
     }
 
+    public function notEditable(bool $notEditable = true): self
+    {
+        $this->notEditable = $notEditable;
+
+        return $this;
+    }
+
     public ?bool $notDeletable {
         get => $this->__get('notDeletable');
         set {
             $this->__set('notDeletable', $value);
         }
+    }
+
+    public function notDeletable(bool $notDeletable = true): self
+    {
+        $this->notDeletable = $notDeletable;
+
+        return $this;
     }
 
     public ?bool $notSortable {
@@ -118,11 +210,25 @@ final class Config extends DynamicPropertiesInterface
         }
     }
 
+    public function notSortable(bool $notSortable = true): self
+    {
+        $this->notSortable = $notSortable;
+
+        return $this;
+    }
+
     public ?bool $notCopyable {
         get => $this->__get('notCopyable');
         set {
             $this->__set('notCopyable', $value);
         }
+    }
+
+    public function notCopyable(bool $notCopyable = true): self
+    {
+        $this->notCopyable = $notCopyable;
+
+        return $this;
     }
 
     public ?bool $notCreatable {
@@ -132,11 +238,25 @@ final class Config extends DynamicPropertiesInterface
         }
     }
 
+    public function notCreatable(bool $notCreatable = true): self
+    {
+        $this->notCreatable = $notCreatable;
+
+        return $this;
+    }
+
     public ?bool $switchToEdit {
         get => $this->__get('switchToEdit');
         set {
             $this->__set('switchToEdit', $value);
         }
+    }
+
+    public function switchToEdit(bool $switchToEdit = true): self
+    {
+        $this->switchToEdit = $switchToEdit;
+
+        return $this;
     }
 
     public ?bool $enableVersioning {
@@ -146,11 +266,25 @@ final class Config extends DynamicPropertiesInterface
         }
     }
 
+    public function enableVersioning(bool $enableVersioning = true): self
+    {
+        $this->enableVersioning = $enableVersioning;
+
+        return $this;
+    }
+
     public ?bool $doNotCopyRecords {
         get => $this->__get('doNotCopyRecords');
         set {
             $this->__set('doNotCopyRecords', $value);
         }
+    }
+
+    public function doNotCopyRecords(bool $doNotCopyRecords = true): self
+    {
+        $this->doNotCopyRecords = $doNotCopyRecords;
+
+        return $this;
     }
 
     public ?bool $doNotDeleteRecords {
@@ -160,6 +294,13 @@ final class Config extends DynamicPropertiesInterface
         }
     }
 
+    public function doNotDeleteRecords(bool $doNotDeleteRecords = true): self
+    {
+        $this->doNotDeleteRecords = $doNotDeleteRecords;
+
+        return $this;
+    }
+
     public ?string $backlink {
         get => $this->__get('backlink');
         set {
@@ -167,39 +308,43 @@ final class Config extends DynamicPropertiesInterface
         }
     }
 
+    public function backlink(string $backlink): self
+    {
+        $this->backlink = $backlink;
+
+        return $this;
+    }
+
     public readonly ConfigCallbacks $callbacks;
 
     public readonly ConfigSql $sql;
 
-    public function __construct(private readonly string $table)
+    public function __construct(string $table)
     {
+        $this->_table = $table;
+
         $this->sql = new ConfigSql($table);
         $this->callbacks = new ConfigCallbacks($table);
     }
 
     public function __get(string $name): mixed
     {
-        return $GLOBALS['TL_DCA'][$this->table]['config'][$name] ?? null;
+        return $GLOBALS['TL_DCA'][$this->_table]['config'][$name] ?? null;
     }
 
     public function __set(string $name, mixed $value): void
     {
-        $GLOBALS['TL_DCA'][$this->table]['config'][$name] = $value;
+        $GLOBALS['TL_DCA'][$this->_table]['config'][$name] = $value;
     }
 
     public function __isset(string $name): bool
     {
-        return isset($GLOBALS['TL_DCA'][$this->table]['config'][$name]);
+        return isset($GLOBALS['TL_DCA'][$this->_table]['config'][$name]);
     }
 
     public function __unset(string $name): void
     {
-        unset($GLOBALS['TL_DCA'][$this->table]['config'][$name]);
-    }
-
-    public function unset(string $name): void
-    {
-        $this->__unset($name);
+        unset($GLOBALS['TL_DCA'][$this->_table]['config'][$name]);
     }
 
     /**
@@ -215,7 +360,7 @@ final class Config extends DynamicPropertiesInterface
 
     public function addCallback(ConfigCallback $name, callable $callback): void
     {
-        $GLOBALS['TL_DCA'][$this->table]['config'][$name->value][] = $callback;
+        $GLOBALS['TL_DCA'][$this->_table]['config'][$name->value][] = $callback;
     }
 
 }
