64857776b7b9412b0f51b28b3b6bfda8c8d0f790
Tastaturberuf <mail@tastaturberuf.de>
2026-01-12 10:55:20 +0100

    Refactor `Listing` to use lazy initialization for `sorting` and `label` properties, replacing direct assignments in the constructor.

--- src/Listing.php
+++ src/Listing.php
@@ -6,25 +6,31 @@ namespace Tastaturberuf\ContaoDataContainerAccessor;
 
 final class Listing extends DynamicPropertiesInterface
 {
+    private readonly string $_table;
 
-    public readonly Sorting $sorting;
-    public readonly Label $label;
+    public private(set) Sorting $sorting {
+        get => $this->sorting ??= new Sorting($this->_table);
+    }
+    public private(set) Label $label {
+        get => $this->label ??= new Label($this->_table);
+    }
 
     public mixed $globalOperations {
-        get => $GLOBALS['TL_DCA'][$this->table]['list']['global_operations'] ?? null;
+        get => $GLOBALS['TL_DCA'][$this->_table]['list']['global_operations'] ?? null;
         set {
-            $GLOBALS['TL_DCA'][$this->table]['list']['global_operations'][] = match (true) {
+            $GLOBALS['TL_DCA'][$this->_table]['list']['global_operations'][] = match (true) {
                 is_callable($value) => $value,
                 $value instanceof GlobalOperation => get_object_vars($value),
-                default => throw new \LogicException('Global operation must be callable or instance of ' . GlobalOperation::class),
+                default => throw new \LogicException('Global operation must be callable or instance of '
+                . GlobalOperation::class),
             };
         }
     }
 
     public mixed $operations {
-        get => $GLOBALS['TL_DCA'][$this->table]['list']['operations'] ?? null;
+        get => $GLOBALS['TL_DCA'][$this->_table]['list']['operations'] ?? null;
         set {
-            $GLOBALS['TL_DCA'][$this->table]['list']['operations'][] = match (true) {
+            $GLOBALS['TL_DCA'][$this->_table]['list']['operations'][] = match (true) {
                 is_callable($value) => $value,
                 $value instanceof Operation => get_object_vars($value),
                 default => throw new \LogicException('Operation must be callable or instance of ' . Operation::class),
@@ -32,35 +38,33 @@ final class Listing extends DynamicPropertiesInterface
         }
     }
 
-    public function __construct(private readonly string $table)
+    public function __construct(string $table)
     {
-        $this->sorting = new Sorting($table);
-        $this->label = new Label($table);
+        $this->_table = $table;
     }
 
     public function __get(string $name): mixed
     {
-        return $GLOBALS['TL_DCA'][$this->table]['list'][$name] ?? null;
+        return $GLOBALS['TL_DCA'][$this->_table]['list'][$name] ?? null;
     }
 
     public function __set(string $name, mixed $value): void
     {
-        $GLOBALS['TL_DCA'][$this->table]['list'][$name] = $value;
+        $GLOBALS['TL_DCA'][$this->_table]['list'][$name] = $value;
     }
 
     public function __isset(string $name): bool
     {
-        return isset($GLOBALS['TL_DCA'][$this->table]['list'][$name]);
+        return isset($GLOBALS['TL_DCA'][$this->_table]['list'][$name]);
     }
 
     public function __unset(string $name): void
     {
-        unset($GLOBALS['TL_DCA'][$this->table]['list'][$name]);
+        unset($GLOBALS['TL_DCA'][$this->_table]['list'][$name]);
     }
 
     public function addCallback(ListCallback $name, callable $callback): void
     {
-        $GLOBALS['TL_DCA'][$this->table]['list']['sorting'][$name->value] = $callback;
+        $GLOBALS['TL_DCA'][$this->_table]['list']['sorting'][$name->value] = $callback;
     }
-
 }
