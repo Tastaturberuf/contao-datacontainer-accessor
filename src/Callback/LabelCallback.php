2862b89d141ddc217e4672b138788dfa0e8b02b8
Tastaturberuf <mail@tastaturberuf.de>
2026-04-12 09:10:40 +0200

    add `LabelCallback` method: encapsulate label callback creation with `Closure` support and annotations for improved analysis

--- src/Callback/LabelCallback.php
+++ src/Callback/LabelCallback.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace Tastaturberuf\ContaoDataContainerAccessor\Callback;
 
+use Closure;
+
 enum LabelCallback: string
 {
-
     case Group = 'group_callback';
     case Label = 'label_callback';
 
+    /**
+     * @mago-expect analysis:mixed-array-assignment
+     * @mago-expect lint:no-global
+     */
+    public function create(string $table, Closure $callback): void
+    {
+        $GLOBALS['TL_DCA'][$table]['list']['label'][$this->value] = $callback;
+    }
 }
